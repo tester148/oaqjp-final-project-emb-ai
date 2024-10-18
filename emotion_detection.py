38769ad4d025abfad356cc9e0ff48d06e2d07c5f
tester148 <genx21900@gmail.com>
2024-10-18 17:13:08 +0000

    cd

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,21 +1,28 @@
 import requests
 
 def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = {
-        "raw_document": {
-            "text": text_to_analyze}}
-    try:
-        response = requests.post(url, headers=headers, json=input_json)
-        response.raise_for_status() 
-        response_json = response.json()
-        print("Response JSON:", response_json) # i am outpuying
-        emotions = response_json.get('emotion', {})  
-        return emotions
-
-    except requests.exceptions.RequestException as e:
-        return f"Request failed: {e}"
-
-
+   url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+   headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+   input_json = {"raw_document": {"text": text_to_analyze}}
+   response = requests.post(url, headers=headers, json=input_json)
+   if response.status_code == 200:
+        data = response.json()
+        emotions = data.get('emotion', {})
+        anger_score = emotions.get('anger', 0)
+        disgust_score = emotions.get('disgust', 0)
+        fear_score = emotions.get('fear', 0)
+        joy_score = emotions.get('joy', 0)
+        sadness_score = emotions.get('sadness', 0)
+        emotion_scores = {'anger': anger_score,'disgust': disgust_score, 'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,}
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+        return {'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+        }
+   else:
+        return {"error": "Unable to fetch emotions"}, 500
